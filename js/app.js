4b9aa27cffeafb64742488a4fc8b861fef35a62d
Jaudat Syed <sjh.jaudat@gmail.com>
2016-06-06 16:59:48 -0400

    Started work, need to position the Enemy and Player properly on the gameboard

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+//Enums containing the values of various constants used throughout the cpde
+var gameboard = {
+    NUM_OF_ROWS: 6,
+    NUM_OF_COLS: 5,
+    ROW_WIDTH: 70,
+    COL_WIDTH: 101
+}; 
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -6,6 +14,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 0 * gameboard.COL_WIDTH; //column
+    this.y = 3 * gameboard.ROW_WIDTH; //row
 };
 
 // Update the enemy's position, required method for game
@@ -21,15 +31,42 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// Returns a random integer between min (included) and max (excluded)
+// Using Math.round() will give you a non-uniform distribution!
+Enemy.prototype.getStartingRow = function() {
+    var row1 = 1;
+    var row2 = 4;
+    return Math.floor( Math.random()*(max-min) ) + min
+};
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+
+var Player = function() {
+    this.sprite = 'images/char-cat-girl.png';
+    this.x = 2 * gameboard.COL_WIDTH; //column
+    this.y = 5 * gameboard.ROW_WIDTH; //row
+};
+
+Player.prototype.update = function() {
+    //TODO: Finish function
+};
+
+Player.prototype.render = function() {
+    //TODO: Finish function
+};
+
+Player.prototype.handleInput = function() {
+    //TODO: Finish function
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [new Enemy(), new Enemy(), new Enemy()];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
